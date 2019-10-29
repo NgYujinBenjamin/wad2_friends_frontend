92b41d3441f7f4d62dc0afa07970031ec592df5e
Joel-David <joel@joeldavidw.com>
2019-10-29 12:15:10 +0800

    Added Azure Translation Example

--- pages/feed.vue
+++ pages/feed.vue
@@ -8,6 +8,7 @@
         <b-card class="feed-card" :img-src="article.urlToImage" img-alt="Card image" img-top :title="article.title">
           <b-card-text v-text="article.description">
           </b-card-text>
+          <b-button @click="translate(article.description)">Translate Test</b-button>
         </b-card>
       </div>
     </masonry>
@@ -26,6 +27,30 @@
         async asyncData({$axios}) {
             const articles = await $axios.$get('https://newsapi.org/v2/top-headlines?country=sg&apiKey=7b8d0f9048464a8fa74e3edf2c215b8d').then(res => res.articles)
             return {articles}
+        },
+        methods: {
+            translate: function (description) {
+                console.log(description)
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Ocp-Apim-Subscription-Key': 'd9e003958b7244daa92f6cd96ce39cdb'
+                    }
+                }
+
+                let url = `https://api-apc.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=de`;
+                this.$axios.post(url,
+                    [
+                        {"text": description}
+                    ], config
+                ).then(response => {
+                        let translated = response.data[0]['translations']
+                        alert(JSON.stringify(translated))
+                    }
+                ).catch(e => {
+                    console.log(e)
+                })
+            }
         }
     }
 </script>
