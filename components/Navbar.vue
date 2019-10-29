ead6c3017268606f384d749064d595bc4ef9b608
Benjamin <benjaminng.2017@sis.smu.edu.sg>
2019-10-29 20:59:18 +0800

    added nav bar changes
    nav bar changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,7 +7,8 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/feed">News Feed</b-nav-item>
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
