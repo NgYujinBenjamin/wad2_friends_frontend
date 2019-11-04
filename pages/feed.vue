c106d68ffb98441d8dead05d6412d96b3397ad0b
tukiznaj <najulah1996@hotmail.com>
2019-11-04 21:54:58 +0800

    added modals with independant language translation

--- pages/feed.vue
+++ pages/feed.vue
@@ -6,13 +6,57 @@
     >
       <div v-for="(article, index) in articles" :key="index">
 
-        <b-card class="feed-card" :img-src="article.urlToImage" img-alt="Card image" img-top :title="article.title">
-          <b-card-text v-text="article.description">
-          </b-card-text>
-          <div v-for="language in languages">
-            <button class="btn-outline-success" @click="translate(article.description,language.code)"><span
-              v-text="language.desc"></span></button>
+        <b-card class="feed-card" :img-src="article.urlToImage" img-alt="Article Image" img-top :title="article.title">
+          <b-card-text v-text="article.description" v-model="index"></b-card-text>
+
+          <div class="row mx-1">
+            <a href="#" @click.prevent="redirect(article.url)" class="card-link d-inline pull-left">
+              Read Full Article
+            </a>
+            <b-button v-b-modal="'myModal' + index" class="card-link d-inline pull-right" variant="success" size="sm">
+              Share
+            </b-button>
           </div>
+
+          <b-modal v-bind:id="'myModal'+index" :title="'Article '+index">
+
+            <template v-slot:modal-header="{ close }">
+              <!-- Emulate built in modal header close button action -->
+              <h6>{{article.title}}</h6>
+            </template>
+
+            <template>
+              <b-card-text v-text="article.description" v-model="index"></b-card-text>
+              <img alt="Article Image" :src="article.urlToImage" class="w-100"/>
+            </template>
+
+            <template v-slot:modal-footer="{ ok, cancel, hide }">
+              <b-dropdown id="dropdown-offset" offset="25" size="sm" text="Translate" class="m-2">
+                <b-dropdown-item v-for="language in languages" @click.prevent="translate(index,language.code)">
+                  {{language.desc}}
+                </b-dropdown-item>
+                <!--                <option v-model="selected" v-for="language in languages" :value="language.code">{{language.desc}}</option>-->
+              </b-dropdown>
+
+              <b-button size="sm" variant="danger" @click="cancel()">
+                Cancel
+              </b-button>
+
+              <b-button size="sm" variant="success" @click="ok()">
+                Post
+              </b-button>
+
+
+            </template>
+          </b-modal>
+
+          <!--          <div v-for="language in languages">-->
+          <!--            <button class="btn-outline-success" @click="translate(article.description,language.code)">-->
+          <!--              <span v-text="language.desc"></span>-->
+          <!--            </button>-->
+          <!--          </div>-->
+
+
         </b-card>
       </div>
     </masonry>
@@ -48,26 +92,26 @@
             // mounted will run functions after page is loaded
             let currURL = document.location.href;
             let params = currURL.split("?");
-            console.log("params", params, params.length);
+            // console.log("params", params, params.length);
+            let language = "en";
             let url = "";
 
             // check if there is any parameters from index.vue
             if (params.length > 1) {
+                language = params[1].split("language=")[1];
                 params = params[1];
                 url = 'https://newsapi.org/v2/everything?apiKey=7b8d0f9048464a8fa74e3edf2c215b8d&' + params;
-                console.log("params", params, params.length);
             } else {
                 // if feed page is visited directly without any parameters
                 // use the headlines url instead
                 url = "https://newsapi.org/v2/top-headlines?country=sg&apiKey=7b8d0f9048464a8fa74e3edf2c215b8d";
             }
-
-            console.log("params", params, params.length);
-
-            this.fetchData(url);
+            
+            // console.log(params,language);
+            this.fetchData(url, language);
         },
         methods: {
-            translate: function (description, language) {
+            translate: function (index, language) {
                 const config = {
                     headers: {
                         'Content-Type': 'application/json',
@@ -75,24 +119,35 @@
                     }
                 };
 
-                let url = 'https://api-apc.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=' + language;
+                let currLanguage = this.articles[index].language;
+                let url = 'https://api-apc.cognitive.microsofttranslator.com/translate?api-version=3.0&from=' + currLanguage + '&to=' + language;
                 this.$axios.post(url,
                     [
-                        {"text": description}
+                        {"text": this.articles[index].description}
                     ], config
                 ).then(response => {
                         let translated = response.data[0]['translations'];
-                        alert(JSON.stringify(translated))
+                        this.articles[index].description = translated[0].text;
+                        this.articles[index].language = language;
+                        // console.table(translated[0].text, language, currLanguage);
                     }
                 ).catch(e => {
                     console.log(e);
                 })
             },
-            async fetchData(url) {
+            redirect(url) {
+                // console.log(url);
+                window.open(url, '_blank');
+            },
+            async fetchData(url, language) {
                 const res = await this.$axios.$get(url);
-                // console.log(typeof res);
-                console.log(res.articles);
                 this.articles = res.articles;
+
+                for (let i = 0; i < this.articles.length; i++) {
+                    // set the language property for each article for translation use later on
+                    this.articles[i].language = language;
+                }
+
             }
         }
     }
