775d874ed722e506bad1f12dbc42ad8ef19769aa
linhanhui <hanhui.lin.2017@sis.smu.edu.sg>
2019-11-05 18:11:02 +0800

    Revert "Revert "Revert "Revert "Merge pull request #1 from joeldavidw/LoginPage""""
    This reverts commit 3fdfd112d09c51ba330457ed6f9381094b153de7.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -32,7 +32,7 @@
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#" @click='logout()'>Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -41,11 +41,16 @@
 </template>
 
 <script>
-    export default {
-        name: "navbar"
+export default {
+  name: "navbar",
+  methods: {
+    logout() {
+      localStorage.removeItem("jwt");
+      this.$router.replace({ name: "login" });
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -21,36 +21,41 @@
               id="LoginPassword"
               class="form-control w-auto"
             />
-            <button class="btn" type="password" style="position:absolute;right:0px;" @click="switchVisibility">
+            <button
+              class="btn"
+              type="password"
+              style="position:absolute;right:0px;"
+              @click="switchVisibility"
+            >
               <i class="far fa-eye"></i>
             </button>
           </div>
         </div>
         <div class="text-center mt-4 mb-3">
-          <button class="btn btn-info w-100">Login</button>
-          <p class="text-right mt-1"><a href="#">Forget password?</a></p>
+          <button class="btn btn-info w-100" @click="login()">Login</button>
+          <p class="text-right mt-1">
+            <a href="#">Forget password?</a>
+          </p>
         </div>
 
         <div style="height: 20px; border-bottom: 1px solid #616161; text-align: center">
-          <span style="font-size: 20px; color:#616161; background-color: #F3F5F6; padding: 0 10px;">
-            OR
-          </span>
+          <span
+            style="font-size: 20px; color:#616161; background-color: #F3F5F6; padding: 0 10px;"
+          >OR</span>
         </div>
         <p class="text-center mt-4" style="color:#616161;">
           <strong>
             Need an account ?
-            <a href="#">Sign up here</a>
+            <a @click="this.$router.push('/signup')">Sign up here</a>
           </strong>
         </p>
       </div>
     </div>
-    <!-- <p class="text-center mt-5 text-danger" v-if="showErrorMsg">Invalid username/password</p> -->
   </div>
 </template>
 <script>
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-// import VueJwtDecode from "vue-jwt-decode";
 
 export default {
   name: "Login",
@@ -58,8 +63,7 @@ export default {
     return {
       username: "",
       password: "",
-      passwordFieldType: "password",
-      // showErrorMsg: localStorage.getItem("errormsg")
+      passwordFieldType: "password"
     };
   },
   methods: {
@@ -67,37 +71,31 @@ export default {
       this.passwordFieldType =
         this.passwordFieldType === "password" ? "text" : "password";
     },
-    validUsername() {
-      return this.username == "" ? false : true;
-    },
     login() {
-      if (this.validUsername()) {
-        // axios
-        //   .get(
-        //     "api" +
-        //       this.username
-        //   )
-        //   .then(function(response) {
-        //     var token = response.data.response;
-        //     var userInfo = JSON.stringify(VueJwtDecode.decode(token));
-        //     localStorage.setItem("user", userInfo);
-        //     localStorage.setItem("errormsg", "");
-        //   })
-        //   .catch(error => {
-        //     this.showErrorMsg = true;
-        //     //eslint-disable-next-line no-console
-        //     console.log(error);
-        //     localStorage.setItem("errormsg", true);
-        //     localStorage.setItem("user", false);
-        //   })
-        //   .finally(() => {
-        //     if (localStorage.getItem("errormsg") != "true") {
-        //       this.$router.push({ name: "home" });
-        //     }
-        //   });
-      } else {
-        this.showErrorMsg = true;
-      }
+      this.$axios
+        .post(process.env.baseUrl + "/auth/local", {
+          identifier: this.username,
+          password: this.password
+        })
+        .then(function(response) {
+          localStorage.setItem("jwt", response.data.jwt);
+          localStorage.removeItem("errormsg");
+        })
+        .catch(error => {
+          this.$toast.error("Invalid username/password", {
+            icon: { name: "exclamation-triangle" }
+          });
+        })
+        .finally(() => {
+          if (localStorage.getItem("jwt")) {
+            this.$router.push({ name: "index" });
+          }
+        });
+    }
+  },
+  mounted: function() {
+    if (localStorage.getItem("jwt")) {
+      this.$router.replace({ name: "index" });
     }
   }
 };
