cc658bb1723caed51ce8efc09b76088c27656ef9
tukiznaj <najulah1996@hotmail.com>
2019-11-08 23:56:30 +0800

    Fixed Carousel

--- components/Carousel/Carousel.vue
+++ components/Carousel/Carousel.vue
@@ -1,38 +1,32 @@
 <template>
-    <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="3000">
-        <ol class="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel" data-slide-to="1"></li>
-            <li data-target="#carousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100 h-100" :src="carouselArticles[0]? carouselArticles[0].urlToImage : 'https://membershipdrive.com/wp-content/uploads/2014/06/placeholder.png'" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100 h-100" :src="carouselArticles[1]? carouselArticles[1].urlToImage : 'https://membershipdrive.com/wp-content/uploads/2014/06/placeholder.png'" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100 h-100" :src="carouselArticles[2]? carouselArticles[2].urlToImage : 'https://membershipdrive.com/wp-content/uploads/2014/06/placeholder.png'" alt="Third slide">
-            </div>
-        </div>
-        <!-- left and right controls (not working for now)-->
-        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" ></span>
-                <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-                <span class="sr-only">Next</span>
-        </a>
-    </div>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        v-for="(article,index) in carouselArticles"
+        :key="index"
+        :caption="article.Title"
+        :text="article.description"
+        :img-src="article.urlToImage"
+      ></b-carousel-slide>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
 export default {
     data() {
             return {
-                carouselArticles: {},
+                carouselArticles: {}
             };
         },
     methods: {
