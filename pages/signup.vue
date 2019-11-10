f17910209190e0ad0926b7b768e09f115731e683
Joel-David <joel@joeldavidw.com>
2019-11-10 23:42:51 +0800

    Fixed signup issue

--- pages/signup.vue
+++ pages/signup.vue
@@ -7,24 +7,24 @@
           <strong>Sign Up</strong>
         </p>
         <label for="Email" class="font-weight-bold" style="color:#616161;">Your E-mail address</label>
-        <input type="email" v-model="email" id="Email" class="form-control" required />
+        <input type="email" v-model="email" id="Email" class="form-control" required/>
 
         <label for="Username" class="font-weight-bold" style="color:#616161;">Your username</label>
-        <input v-model="username" id="Username" class="form-control" required />
+        <input v-model="username" id="Username" class="form-control" required/>
 
         <label
           for="Password"
           class="grey-text font-weight-bold d-block mt-2"
           style="color:#616161;"
         >Your password</label>
-        <input v-model="password" id="Password" class="form-control" />
+        <input v-model="password" id="Password" type="password" class="form-control"/>
 
         <label
           for="CfmPassword"
           class="grey-text font-weight-bold d-block mt-2"
           style="color:#616161;"
         >Confirm your password</label>
-        <input v-model="cfmPassword" id="CfmPassword" class="form-control" />
+        <input v-model="cfmPassword" id="CfmPassword" type="password" class="form-control"/>
 
         <div class="text-center mt-4 mb-3">
           <button class="btn btn-info w-100" @click="signup()">Register</button>
@@ -46,67 +46,68 @@
   </div>
 </template>
 <script>
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
+    import "@fortawesome/fontawesome-free/css/all.css";
+    import "@fortawesome/fontawesome-free/js/all.js";
 
-export default {
-  name: "Signup",
-  data() {
-    return {
-      email: "",
-      username: "",
-      password: "",
-      cfmPassword: ""
-    };
-  },
-  methods: {
-    signup() {
-      if (this.checkForm()) {
-        this.$axios
-          .post(process.env.baseUrl + "/auth/local/register", {
-            username: this.username,
-            email: this.email,
-            password: this.password
-          })
-          .then(response => {
-            localStorage.setItem("jwt", response.data.jwt);
-            this.$toast.success("Succussful registration!", {
-              icon: { name: "check-circle" }
-            });
-          })
-          .catch(error => {
-            this.$toast.error("Something has went wrong....", {
-              icon: { name: "bug" }
-            });
-          })
-          .finally(() => {
-            if (localStorage.getItem("jwt")) {
-              this.$router.replace({ name: "index" });
+    export default {
+        name: "Signup",
+        data() {
+            return {
+                email: "",
+                username: "",
+                password: "",
+                cfmPassword: ""
+            };
+        },
+        methods: {
+            signup() {
+                if (this.checkForm()) {
+                    this.$axios
+                        .post(process.env.baseUrl + "/auth/local/register", {
+                            username: this.username,
+                            email: this.email,
+                            password: this.password
+                        })
+                        .then(response => {
+                            localStorage.setItem("jwt", response.data.jwt);
+                            localStorage.setItem("user", JSON.stringify(response.data.user));
+                            this.$toast.success("Succussful registration!", {
+                                icon: {name: "check-circle"}
+                            });
+                        })
+                        .catch(error => {
+                            this.$toast.error("Something has went wrong....", {
+                                icon: {name: "bug"}
+                            });
+                        })
+                        .finally(() => {
+                            if (localStorage.getItem("jwt")) {
+                                this.$router.replace({name: "index"});
+                            }
+                        });
+                } else {
+                    this.$toast.error("Password and confirm password does not match!", {
+                        icon: {name: "exclamation-triangle"}
+                    });
+                }
+            },
+            checkForm() {
+                if (this.password !== this.cfmPassword) {
+                    return false;
+                }
+                return true;
             }
-          });
-      } else {
-        this.$toast.error("Password and confirm password does not match!", {
-          icon: { name: "exclamation-triangle" }
-        });
-      }
-    },
-    checkForm() {
-      if (this.password !== this.cfmPassword) {
-        return false;
-      }
-      return true;
-    }
-  },
-  mounted: function() {
-    if (localStorage.getItem("jwt")) {
-      this.$router.replace({ name: "index" });
-    }
-  }
-};
+        },
+        mounted: function () {
+            if (localStorage.getItem("jwt")) {
+                this.$router.replace({name: "index"});
+            }
+        }
+    };
 </script>
 
 <style scoped>
-body {
-  background-color: white !important;
-}
+  body {
+    background-color: white !important;
+  }
 </style>
