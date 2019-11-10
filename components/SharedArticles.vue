d41983bbf0775f87d524742deff92de42910825b
tukiznaj <najulah1996@hotmail.com>
2019-11-10 11:42:55 +0800

    added recommendations

---
+++ components/SharedArticles.vue
@@ -0,0 +1,359 @@
+<template>
+  <div>
+    <!--   previous b-carousel grey colour: #eceff1-->
+    <div v-if="articles.length > 0">
+      <b-carousel
+        :interval="100000"
+        controls
+        indicators
+        background="#fffff"
+        style="text-shadow: 1px 1px 2px #333;"
+        id="fullscreen"
+      >
+      <span v-for="(article,index) in articles" :key="article.id">
+        <b-carousel-slide
+          img-src="~/assets/white_bg.png"
+          class="image_sliders"
+          v-if="index%3 === 0"
+        >
+          <div class="row">
+            <shared-article-card class="d-inline halfCard" v-bind:article="article" v-bind:article-width="30"
+                                 v-bind:hover-text="'Read Article'" v-bind:type="'full'"
+                                 v-bind:key="article.url"></shared-article-card>
+            <shared-article-card class="d-inline halfCard" v-bind:article="articles[index+1]" v-bind:article-width="30"
+                                 v-bind:hover-text="'Read Article'" v-bind:type="'full'"
+                                 v-bind:key="articles[index+1].url"></shared-article-card>
+            <shared-article-card class="d-inline halfCard" v-bind:article="articles[index+2]" v-bind:article-width="30"
+                                 v-bind:hover-text="'Read Article'" v-bind:type="'full'"
+                                 v-bind:key="articles[index+2].url"></shared-article-card>
+          </div>
+        </b-carousel-slide>
+      </span>
+      </b-carousel>
+
+      <b-carousel
+        :interval="400000"
+        controls
+        indicators
+        background="#ffffff"
+        id="halfscreen"
+      >
+      <span v-for="(article,index) in articles" :key="article.id">
+        <b-carousel-slide
+          img-src="~/assets/white_bg.png"
+          class="image_sliders"
+          v-if="index%2 === 0"
+        >
+          <div class="row">
+            <shared-article-card class="d-inline halfCard" v-bind:article="article" v-bind:article-width="45"
+                                 v-bind:hover-text="'Read Article'" v-bind:type="'half'"
+                                 v-bind:key="article.url"></shared-article-card>
+            <shared-article-card class="d-inline halfCard" v-bind:article="articles[index+1]" v-bind:article-width="45"
+                                 v-bind:hover-text="'Read Article'" v-bind:type="'half'"
+                                 v-bind:key="articles[index+1].url"></shared-article-card>
+          </div>
+        </b-carousel-slide>
+      </span>
+      </b-carousel>
+
+      <b-carousel
+        :interval="4000"
+        controls
+        indicators
+        background="#ffffff"
+        id="mobile"
+      >
+        <b-carousel-slide
+          img-src="~/assets/white_bg.png"
+          class="image_sliders"
+          v-for="(article,index) in articles"
+          :key="index"
+        >
+          <shared-article-card class="mobileCard" v-bind:article="article" v-bind:article-width="100"
+                               v-bind:hover-text="'Read Full Article'" v-bind:type="'mobile'"
+                               v-bind:key="article.url"></shared-article-card>
+        </b-carousel-slide>
+      </b-carousel>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    import "@fortawesome/fontawesome-free/css/all.css";
+    import SharedArticleCard from "../components/feed/shared-article-card";
+
+    export default {
+        name: "sharedArticles",
+        components: {SharedArticleCard},
+        data: function () {
+            return {
+                slides: 3,
+                articles: [],
+                // get latest set of 12 articles shared
+                latest_shared_limit: 12,
+                savedMsg: "Bookmark news",
+                latest_sort_col: "id",
+                latest_sort_order: "DESC", // descending order;
+
+                model: 0,
+                showArrows: false,
+                hideDelimiters: false,
+                cycle: true,
+
+                // for profile
+                imgSet: require('@/assets/noimageavailable.png'),
+
+                saved: false,
+                pageLoader: true,
+                msg: "Loading...",
+                savedmsg: "Bookmark News",
+                languages: [
+                    {code: "en", desc: "English", isActive: false},
+                    {code: "es", desc: "Spanish/español", isActive: false},
+                    {code: "ar", desc: "Arabic/عربى", isActive: false},
+                    {code: "zh", desc: "Chinese/中文", isActive: false}
+                ],
+            }
+        },
+        mounted: function () {
+            // mounted will run functions after page is loaded
+            let bearerToken = localStorage.getItem("jwt") ? localStorage.getItem("jwt") : this.$router.replace({name: "login"});
+            let keywords = localStorage.getItem("search_keywords") ? localStorage.getItem("search_keywords") : "null";
+            console.log("current keywords", keywords);
+
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + bearerToken
+                }
+            };
+
+            let url = "https://sa-api.eof.cx/articles?_limit=" + this.latest_shared_limit + "&_sort=" + this.latest_sort_col + ":" + this.latest_sort_order;
+            this.fetchSharedArticles(url, config);
+        },
+        methods: {
+            async fetchSharedArticles(url, config) {
+                console.table(url, config);
+                let user = JSON.parse(localStorage.getItem("user"));
+                const res = await this.$axios.$get(url, config);
+
+                let jwt = localStorage.getItem("jwt");
+                const headers = {
+                    'Authorization': 'Bearer ' + jwt
+                };
+
+                const savedArticles = await this.$axios.$get("https://sa-api.eof.cx/savedarticles?user=" + user.id, {
+                    headers: headers
+                });
+
+                let _articles = res;
+
+                for (let i = 0; i < _articles.length; i++) {
+                    _articles[i].language = res.language == null ? "en" : res.language;
+                    _articles[i].uniqueid = user.id + "|" + _articles[i].url
+                    _articles[i].saved = false;
+                }
+
+                for (let y = 0; y < savedArticles.length; y++) {
+                    for (let i = 0; i < _articles.length; i++) {
+                        if (_articles[i].uniqueid === savedArticles[y].uniqueid) {
+                            _articles[i].saved = true;
+                            _articles[i].savedid = savedArticles[y].id
+                        }
+                    }
+                }
+
+                for (let i = 0; i < _articles.length; i++) {
+                    this.articles.push(_articles[i])
+                }
+
+                console.log(this.articles);
+            },
+            redirect(url) {
+                // console.log(url);
+                window.open(url, "_blank");
+            },
+            saveNews(i) {
+                let user = JSON.parse(localStorage.getItem("user"));
+                let jwt = localStorage.getItem("jwt");
+
+                let article = this.articles[i];
+                article.saved = !article.saved;
+                this.$set(this.articles, i, article);
+
+                const headers = {
+                    'Authorization': 'Bearer ' + jwt
+                };
+
+                if (article.saved) {
+                    this.savedMsg = "Bookmarked";
+                    article.saveMessage = "Bookmarked";
+
+                    let data = {
+                        "url": article.url,
+                        "urlToImage": article.urlToImage,
+                        "title": article.title,
+                        "description": article.description,
+                        "user": user.id,
+                        "display": true,
+                        "uniqueid": user.id + "|" + article.url
+                    };
+
+                    this.$axios
+                        .post(process.env.baseUrl + "/savedarticles",
+                            data,
+                            {
+                                headers: headers
+                            })
+
+                } else {
+                    this.$axios
+                        .delete(process.env.baseUrl + "/savedarticles/" + article.savedid,
+                            {
+                                headers: headers
+                            });
+                    this.savedMsg = "Bookmark";
+                    article.saveMessage = "Bookmark";
+                }
+            },
+            translate: function (index, language) {
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Ocp-Apim-Subscription-Key": "d9e003958b7244daa92f6cd96ce39cdb"
+                    }
+                };
+
+                let currLanguage = this.articles[index].language;
+                let url =
+                    "https://api-apc.cognitive.microsofttranslator.com/translate?api-version=3.0&from=" +
+                    currLanguage +
+                    "&to=" +
+                    language;
+                this.$axios
+                    .post(url, [{text: this.articles[index].description}], config)
+                    .then(response => {
+                        let translated = response.data[0]["translations"];
+                        this.$set(this.articles[index], 'description', translated[0].text);
+                        this.$set(this.articles[index], 'language', language);
+                    })
+                    .catch(e => {
+                        console.log(e);
+                    });
+            },
+            postToFB: function (msg, link) {
+                let url = "https://graph.facebook.com/112606970182817/feed";
+                const token = "EAAHsGZAfkZA5MBAFQkVnEWb6bnS3lFRHMtLfvufQt9r2NJwAYspX6JBSZCA1WxZAMmh2QJZCJET4pZAA2kiDEsP8KM08jwAjnG26PZCdWNSybD75fO6dAGMZC7HX6yJSkVkgxr7uzYx1DmTUxWJzMsFLDPRElY8qZBy0QwdmfQFUYvdAWmE0LIBLovnThiLnPtmZBjGsb0MkaDFESkHgvc1mbZB";
+                let params = "?message=" + msg + "&link=" + link + "&access_token=" + token;
+                url += params;
+
+                this.$axios
+                    .post(url, [])
+                    .then(response => {
+                        this.$toast.success("Post Successfully Posted", {
+                            icon: {name: "check-circle"}
+                        });
+                    })
+                    .catch(e => {
+                        this.$toast.error("Error: " + e.message, {
+                            icon: {name: "exclamation-triangle"}
+                        });
+                    });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Open%20Sans');
+
+  * {
+    font-family: "Open Sans", serif;
+  }
+
+  .image_sliders {
+    width: 100%;
+    height: 350px;
+    max-height: 350px !important;
+  }
+
+  /*#fullscreen .card-shared {*/
+  /*  width: 31%;*/
+  /*}*/
+
+  #fullscreen img.card-img {
+    width: 100%;
+    height: 211.19px;
+  }
+
+  #halfscreen .card-shared {
+    width: 48%;
+    margin-left: 1%;
+    margin-right: 1%;
+  }
+
+  #halfscreen img.card-img {
+    width: 100%;
+    height: 211.19px;
+  }
+
+  .halfCard.overallCard {
+    /*border: 1px solid red;*/
+    height: 250px;
+  }
+
+  .halfCard.overallCard.cardTextBlock.card-title {
+    border: 1px solid red !important;
+  }
+
+  #mobile .image_sliders {
+    height: 500px;
+    max-height: 500px !important;
+  }
+
+  @media only screen and (max-width: 4096px) {
+    #fullscreen {
+      display: block !important;
+    }
+
+    #halfscreen {
+      display: none !important;
+    }
+
+    #mobile {
+      display: none !important;
+    }
+  }
+
+  @media only screen and (max-width: 1000px) {
+    #fullscreen {
+      display: none !important;
+    }
+
+    #halfscreen {
+      display: block !important;
+    }
+
+    #mobile {
+      display: none !important;
+    }
+  }
+
+
+  @media only screen and (max-width: 700px) {
+    #fullscreen {
+      display: none !important;
+    }
+
+    #halfscreen {
+      display: none !important;
+    }
+
+    #mobile {
+      display: block !important;
+    }
+  }
+
+</style>
---
+++ components/feed/shared-article-card.vue
@@ -0,0 +1,273 @@
+<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card class="ml-3 my-3 overallCard" :style="{'width':articleWidth+'%'}">
+      <!-- image -->
+      <v-img :aspect-ratio="16/9" :src="article.urlToImage == null ? imgSet : article.urlToImage">
+        <!-- hover effect -->
+        <v-expand-transition>
+          <a v-if="hover" class="d-flex transition-fast-in-fast-out v-card--reveal card-link"
+             :href="article.url" target="_blank">
+            {{hoverText}}
+          </a>
+        </v-expand-transition>
+      </v-img>
+
+      <v-card-text style="position: relative;" class="cardTextBlock">
+        <!-- Bookmark -->
+        <v-btn absolute class="articleButton mr-2" color="whitesmoke" fab medium right top
+               @click="saveNews()">
+          <i :class="saved ? 'fas fa-heart fa-lg' : 'far fa-heart fa-lg'"></i>
+        </v-btn>
+        <!-- Share -->
+        <v-btn absolute color="whitesmoke" fab right top v-b-modal="type+article.url">
+          <i class="fas fa-share-alt fa-lg"></i>
+        </v-btn>
+        <!-- Details -->
+        <em class="font-weight-normal title mb-2 publisher" v-text="article.publisher"></em>
+        <h3 class="mb-2 card-title text-left"
+            v-text="article.title.lastIndexOf(' - ') === -1 ? article.title : article.title.substring(0, article.title.lastIndexOf(' - '))"></h3>
+        <!--        <div class="font-weight-normal title mb-2 card-title card-desc" v-text="article.description"></div>-->
+      </v-card-text>
+
+      <!-- Facebook Popup -->
+      <b-modal v-bind:id="type+article.url" header-bg-variant="primary"
+               header-text-variant="light" style="z-index: 999">
+        <template v-slot:modal-header="{ close }">
+          <h5><i class="fab fa-facebook-square fa-lg"></i> Share on Facebook</h5>
+        </template>
+
+        <template>
+          <textarea style="width: 100%; min-height: 150px;" v-text="article.description"></textarea>
+          <img alt="Article Image" :src="article.urlToImage" class="w-100"/>
+        </template>
+
+        <template v-slot:modal-footer="{ ok, cancel, hide }">
+          <b-dropdown id="dropdown-offset" variant="outline-info" offset="25" size="sm" text="Translate" class="m-2">
+            <b-dropdown-item v-for="(language, index_lang) in languages" :key="index_lang"
+                             @click.prevent="translate(language.code)">
+              {{language.desc}}
+            </b-dropdown-item>
+          </b-dropdown>
+          <b-button size="sm" variant="outline-secondary" @click="cancel()">Cancel</b-button>
+          <b-button size="sm" variant="primary" @click="ok(), postToFB(article.description, article.url)">
+            Post to Facebook
+          </b-button>
+        </template>
+      </b-modal>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+    export default {
+        name: "article-card",
+        props: ['article', 'articleWidth', 'hoverText', 'type'],
+        data() {
+            return {
+                saved: this.article.saved,
+                languages: [
+                    {code: "en", desc: "English", isActive: false},
+                    {code: "es", desc: "Spanish/español", isActive: false},
+                    {code: "ar", desc: "Arabic/عربى", isActive: false},
+                    {code: "zh", desc: "Chinese/中文", isActive: false}
+                ]
+            }
+        },
+        methods: {
+            translate: function (language) {
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Ocp-Apim-Subscription-Key": "d9e003958b7244daa92f6cd96ce39cdb"
+                    }
+                };
+
+                let currLanguage = this.article.language;
+                let url =
+                    "https://api-apc.cognitive.microsofttranslator.com/translate?api-version=3.0&from=" +
+                    currLanguage +
+                    "&to=" +
+                    language;
+                this.$axios
+                    .post(url, [{text: this.article.description}], config)
+                    .then(response => {
+                        let translated = response.data[0]["translations"];
+                        this.$set(this.article, 'description', translated[0].text)
+                    })
+                    .catch(e => {
+                        console.log(e);
+                    });
+            },
+
+            redirect(url) {
+                window.open(url, "_blank");
+            },
+            saveNews() {
+                let user = JSON.parse(localStorage.getItem("user"));
+                let jwt = localStorage.getItem("jwt");
+
+                const headers = {
+                    'Authorization': 'Bearer ' + jwt
+                };
+
+                if (!this.article.saved) {
+                    this.$set(this.article, 'saved', true);
+                    let data = {
+                        "url": this.article.url,
+                        "urlToImage": this.article.urlToImage,
+                        "title": this.article.title,
+                        "description": this.article.description,
+                        "user": user.id,
+                        "display": true,
+                        "uniqueid": user.id + "|" + this.article.url,
+                        "language": "en",
+                        "publisher": this.article.publisher
+                    };
+
+                    this.$axios.post(process.env.baseUrl + "/savedarticles", data,
+                        {
+                            headers: headers
+                        }).then(response => {
+                        this.$set(this.article, 'savedid', response.data.id)
+                        this.saved = true
+                    })
+
+                } else {
+                    this.$axios
+                        .delete(process.env.baseUrl + "/savedarticles/" + this.article.savedid,
+                            {
+                                headers: headers
+                            });
+                    this.$set(this.article, 'saved', false)
+                    this.saved = false
+                }
+            },
+            postToFB: function (msg, link) {
+                let url = "fb-api/112606970182817/feed";
+                const token = "EAAHsGZAfkZA5MBAF3uRKqNZCxRZAMnKZAnHnNCnG8oe1aVTBBzjph3k51ELG4ziQkxqIrJWauL5WA22mwNfpLg7IoYRjJOJSJ3wFZAv8DnCbaWwiGNU1yt9Hi4goRsgt9DCHicqdr7dYar1vCQThI1TAaHBfqcYndBZC0y7xevV5zIL8VnFZANh8rD3OQcwz0ngZD";
+                let params = "?message=" + msg + "&link=" + link + "&access_token=" + token;
+                url += params;
+
+                this.$axios
+                    .post(url, [])
+                    .then(response => {
+                        this.$toast.success("Post Successfully Posted", {
+                            icon: {name: "check-circle"}
+                        });
+                    })
+                    .catch(e => {
+                        this.$toast.error("Error: " + e.message, {
+                            icon: {name: "exclamation-triangle"}
+                        });
+                    });
+            },
+        }
+    }
+</script>
+
+<style scoped>
+  .theme--dark.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+    background-color: #48c9b0;
+    color: black;
+    font-weight: 700;
+    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.02), 0px 2px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 5px 0px rgba(0, 0, 0, 0.06);
+  }
+
+  .overallCard {
+    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.02), 0px 2px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 5px 0px rgba(0, 0, 0, 0.06);
+    display: inline-block;
+    height: 400px;
+    overflow: hidden;
+  }
+
+  .card-title {
+    margin-top: 5px;
+    font-weight: 700;
+    font-size: 22px;
+    height: 140px;
+    color: black;
+  }
+
+  .card-link {
+    height: 100%;
+    background-color: whitesmoke;
+    color: black;
+    font-size: 4vh;
+    text-decoration: inherit;
+  }
+
+  .v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .7;
+    position: absolute;
+    width: 100%;
+  }
+
+  .publisher {
+    font-weight: 500;
+    text-transform: uppercase;
+  }
+
+  .articleButton {
+    right: 28%;
+  }
+
+
+  @media only screen and (max-width: 400px) {
+    .articleButton {
+      right: 28%;
+    }
+  }
+
+  @media only screen and (max-width: 600px) and (min-width: 401px) {
+    .articleButton {
+      right: 15%;
+    }
+  }
+
+  @media only screen and (max-width: 700px) {
+    /* for title box adjustment */
+    .card-title {
+      /*border: 1px solid red;*/
+      margin-top: 6%;
+    }
+  }
+
+  @media only screen and (max-width: 800px) and (min-width: 601px) {
+    .articleButton {
+      right: 20%;
+    }
+  }
+
+  @media only screen and (max-width: 900px) and (min-width: 701px) {
+    /* for title box adjustment */
+    .card-title {
+      /*border: 1px solid red;*/
+      margin-top: 7%;
+      font-weight: 600;
+      font-size: 15px;
+    }
+  }
+
+  @media only screen and (max-width: 1000px) and (min-width: 801px) {
+    .articleButton {
+      right: 30%;
+    }
+  }
+
+  @media only screen and (max-width: 4096px) and (min-width: 1001px) {
+    .articleButton {
+      right: 35%;
+    }
+
+    /* for title box adjustment */
+    .card-title {
+      /*border: 1px solid red;*/
+      margin-top: 8%;
+      font-weight: 500;
+      font-size: 15px;
+    }
+  }
+</style>
